b1be863c227df8d8b3c6a06ab516cc69b2e946b9
Fernando Rojas <75703979+Fernando0422@users.noreply.github.com>
2022-06-09 08:15:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,441 @@
+$(document).ready(function(){
+// Children are continually dying at the hands of our corrupt government and politicians who continually cash in their checks from the NRA.
+
+// var children = ["kids", "The youth", "Minors"]
+// var continually = ["often", "repeatedly", "frequently"]
+// var corrupt = ["evil", "unethical", "dishonest", "immoral", "reprobate"]
+// var government = ["country", "nation"]
+
+
+// var sentence1 = `${children[Math.floor(Math.random()*children.length)]} are ${continually[Math.floor(Math.random()*continually.length)]} dying at the hands of our ${corrupt[Math.floor(Math.random()*corrupt.length)]} ${government[Math.floor(Math.random()*government.length)]} and politicians who continually cash in their checks from the NRA.`
+
+
+
+//The “protection” that you think you are providing is damaging the lives of youths throughout the state of Florida, and you will be the reason why. 
+
+
+var occurs = ["happens", "ensues", "transpires", "surfaces", "appears", "emerges", "arises"]
+
+var sentence2 = `The “protection” that you ${thinkSyn[Math.floor(Math.random()*thinkSyn.length)]} you are ${providingSyn[Math.floor(Math.random()*providingSyn.length)]} is ${damagingSyn[Math.floor(Math.random()*damagingSyn.length)]} to the lives of youths ${throughout[Math.floor(Math.random()*throughout.length)]} the state of Florida, and you will be the ${reasonSyn[Math.floor(Math.random()*reasonSyn.length)]} why`
+
+//If you want to protect these children, then do not sign this bill into legislation. 
+
+var wantSyn = ["need", "wish", "demand", "assume"]
+var protectSyn = ["care for", "defend", "conserve", "assure"]
+var signSyn = ["enact", "confirm", "endorse", "ink", "acknowledge", "authorize", "inscribe"]
+var legislationSyn = ["law"]
+
+var sentence3 = `If you ${wantSyn[Math.floor(Math.random()*wantSyn.length)]} to ${protectSyn[Math.floor(Math.random()*protectSyn.length)]} these children, then do not ${signSyn[Math.floor(Math.random()*signSyn.length)]} this bill into ${legislationSyn[Math.floor(Math.random()*legislationSyn.length)]}`
+
+  
+//You are setting a precedent regarding the treatment of LGBTQ+ youths, condemning not only your state’s children, but the children of the United States to a world of discrimination and hate. 
+
+var settingSyn = ["putting", "placing", "putting down", "laying", "settling", "positioning"]
+var precedentSyn = ["model", "example"]
+var regardingSyn = ["in regards to", "with regard to", "about"]
+var condemningSyn = ["damaging", "denouncing", "destroying"]
+var UsSyn = ["US", "USA", "nation"]
+var discriminationSyn = ["bigotry", "favortism", "inequity", "injustice", "intolerance", "prejudice", "unfairness"]
+var hateSyn = ["loathe", "detest", "hatred", "resentment", "aversion", "hostility", "distaste"]
+
+var sentence4 = `You are ${settingSyn[Math.floor(Math.random()*settingSyn.length)]} a ${precedentSyn[Math.floor(Math.random()*precedentSyn.length)]} ${regardingSyn[Math.floor(Math.random()*regardingSyn.length)]} the treatment of LGBTQ+ youths, ${condemningSyn[Math.floor(Math.random()*condemningSyn.length)]} not only your state’s children, but the children of the ${UsSyn[Math.floor(Math.random()*UsSyn.length)]} to a world of ${discriminationSyn[Math.floor(Math.random()*discriminationSyn.length)]} and ${hateSyn[Math.floor(Math.random()*hateSyn.length)]}`
+
+
+//You claim to push your state into a path of the future, but this treatment toward the LGBTQ+ community sets it back decades to a point of discrimination and blatant hate. 
+var claimSyn = ["aledge", "proclaim", "assert", "believe", "call for", "challenge", "declare"]
+var pushSyn = ["propel", "send", "drive", "force", "jolt"]
+var pathSyn = ["pathway", "track", "footpath"]
+var towardSyn = ["with regard to", "respecting", "concerning, to"]
+var decadesSyn = ["years"]
+var discriminationSyn = ["bigotry", "favortism", "inequity", "injustice", "intolerance", "prejudice", "unfairness"]
+var blatantSyn = ["glaring", "obvious", "undisguised", "unconcealed", "open", "transparent", "unmistakable", "shameless"]
+var hateSyn = ["loathe", "detest", "hatred", "resentment", "aversion", "hostility", "distaste"]
+
+var sentence5 = `You ${claimSyn[Math.floor(Math.random()*claimSyn.length)]} to ${pushSyn[Math.floor(Math.random()*pushSyn.length)]} your state into a ${pathSyn[Math.floor(Math.random()*pathSyn.length)]} of the future, but this treatment ${towardSyn[Math.floor(Math.random()*towardSyn.length)]} the LGBTQ+ community sets it back ${decadesSyn[Math.floor(Math.random()*decadesSyn.length)]} to a point of ${discriminationSyn[Math.floor(Math.random()*discriminationSyn.length)]} and ${blatantSyn[Math.floor(Math.random()*blatantSyn.length)]} hate`
+
+
+//If signed into legislation, your warpath will be known world wide and set the reputation of Florida into a spiral and complete tyranny. 
+
+var signedSyn = ["written", "inscribed", "registered", "enlisted"]
+var legislationSyn = ["law", "ratification"]
+var warpathSyn = ["aggression", "animosity", "antagonism", "antipathy", "bitterness", "enmity", "hatred", "estrangement", "silence", "malice"]
+var knownSyn = ["acknowledged", "obvious", "recognized", "noted"]
+var worldwideSyn = ["globally", "everywhere"]
+var completeSyn = ["entire", "exhaustive", "full", "outright", "thorough"]
+var tryannySyn = ["cruelty", "oppression"]
+
+var sentence6 = `If ${signedSyn[Math.floor(Math.random()*signedSyn.length)]} into ${legislationSyn[Math.floor(Math.random()*legislationSyn.length)]}, your ${warpathSyn[Math.floor(Math.random()*warpathSyn.length)]} will be ${knownSyn[Math.floor(Math.random()*knownSyn.length)]} ${worldwideSyn[Math.floor(Math.random()*worldwideSyn.length)]} and set the reputation of Florida into a spiral and ${completeSyn[Math.floor(Math.random()*completeSyn.length)]} ${tryannySyn[Math.floor(Math.random()*tryannySyn.length)]}`
+
+
+//Do not sign this bill into law and prove that Florida is on the right track of history
+var signSyn = ["enact", "confirm", "endorse", "ink", "acknowledge", "authorize", "inscribe"]
+var proveSyn = ["demonstrate"]
+var rightSyn = ["correct", "positive"]
+var trackSyn = ["side", "verge"]
+
+
+var sentence7 = `Do not ${signSyn[Math.floor(Math.random()*signSyn.length)]} this bill into law and prove that Florida is on the ${rightSyn[Math.floor(Math.random()*rightSyn.length)]} ${trackSyn[Math.floor(Math.random()*trackSyn.length)]} of history`
+
+
+  /// stuff for the emails hehe ///
+  var fullLetter2 = `${sentence1}. ${sentence2}. ${sentence3}. ${sentence4}. ${sentence5}. ${sentence6}. ${sentence7}.`
+    var commonAppEmails = [
+      "info@harderforcongress.com","Aaron@FordforNevada.com", "abigail@democraticags.org","andrew.romanoff@andrewromanoff.com",
+      "andrew@andrewgounardes.com",
+      "andrew@andrewlearned.com",
+      "andrew@andrewyang.com",
+      "angela@halaforvirginia.com",
+      "angie@angiefornebraska.com",
+      "anna@annaeshoo4congress.com",
+      "benray@benraylujan.com",
+      "bob_graham@graham.senate.gov",
+      "bob@bobcasey.com",
+      "bob@kerrey.senate.gov",
+      "bri@gretchenwhitmer.com",
+      "cal@calfornc.com",
+      "campaign@angiecraig.com",
+      "campaign@cartwrightcongress.com",
+      "campaign@degette.com",
+      "campaign@electlong.com",
+      "campaign@harleyforcongress.com",
+      "campaign@keithellison.org",
+      "campaign@kennedyforma.com",
+      "campaign@scottpeters.com",
+      "campaign@shariceforcongress.com",
+      "campaign@siegelfortexas.org",
+      "campaign@valdemings.com",
+      "chuck_grassley@grassley.senate.gov",
+      "chuck_hagel@hagel.senate.gov", 
+      "connect@emergeamerica.org", 
+      "connie@mack.senate.gov", 
+      "conrad_burns@burns.senate.gov",
+      "contact@andomforny.com",
+      "contact@dougjones.com",
+      "contact@emergeamerica.org",
+      "contact@hello.hickenlooper.com",
+      "contact@rebeccaforwa.com",
+      "craig@thomas.senate.gov",
+      "data@nighthorse.falcontech.com",
+      "dccc@dccc.org",
+      "debbie@debbieforcongress.com",
+      "democraticparty@democrats.org",
+      "democratslive@democrats.org",
+      "dirk_kempthorne@kempthorne.senate.gov",
+      "donald@valdezforco.com",
+      "donna@donnashalala.com",
+      "email@murkowski.senate.gov",
+      "feedback@act.democrats.org",
+      "frank_lautenberg@lautenberg.senate.gov",
+      "gabrielle@carolyn4congress.com",
+      "gavin@e.gavinnewsom.com",
+      "gavin@gavinnewsom.com",
+      "gcisneros@cisnerosforcongress.com",
+      "GovernorPolis@state.co.us",
+      "Helen@HelenRosenthal.com",
+      "hello@aishawahab.com",
+      "hello@bradpfaff.com",
+      "hello@coraformontana.com",
+      "hello@corybooker.com",
+      "hello@e.welchforvermont.com",
+      "hello@jayinslee.com",
+      "hello@johnfetterman.com",
+      "hello@phillipsforcongress.org",
+      "info@abbieformayor.com",
+      "info@abigailspanberger.com",
+      "info@action.rosenfornevada.com",
+      "Info@alexmorseforcongress.com",
+      "info@alishashelton.com",
+      "info@allenthomascongress.com",
+      "info@allisonrusso.com",
+      "info@alyse4alaska.com",
+      "info@amandastuck.com",
+      "info@amyklobuchar.com",
+      "info@amylkennedy.com",
+      "info@AmyMcGrath.com",
+      "info@andrewcuomo.com",
+      "info@andybeshear.com",
+      "info@andykimforcongress.com",
+      "info@annettetaddeo.com",
+      "info@audreyforcongress.com",
+      "info@ayannapressley.com",
+      "info@azdem.org",
+      "info@backroadspac.com",
+      "info@barbaraleeforcongress.org",
+      "info@beatabbott.com",
+      "info@benmcadamsforcongress.com",
+      "info@benraylujan.com",
+      "info@beraforcongress.com",
+      "info@berniesanders.com",
+      "info@betoorourke.com",
+      "info@betsysweet.com",
+      "info@blueamericaproject.org",
+      "info@bobbyjeffriesforcongress.com",
+      "info@BoldProgressives.org",
+      "info@brindisiforcongress.com",
+      "info@brynneforcongress.com",
+      "info@cadem.org",
+      "info@calfornc.com",
+      "info@carolyn4congress.com",
+      "info@carolynmaloney.com",
+      "info@castenforcongress.com",
+      "info@catherinecortezmasto.com",
+      "info@chaztedescoforcongress.com",
+      "info@cheribeasley.com",
+      "info@chrisbubser.com",
+      "info@chriscoons.com",
+      "info@chrisforgovernor.com",
+      "info@christyforcongress.org",
+      "info@chuckschumer.com",
+      "info@cicilline.com",
+      "info@cindyaxneforcongress.com",
+      "info@cisnerosforcongress.com",
+      "info@colinallred.com",
+      "info@coraformontana.com",
+      "info@coribush.org",
+      "info@ctdems.org",
+      "info@debbiewassermanschultz.com",
+      "info@delgadoforcongress.com",
+      "info@democraticags.org",
+      "info@democraticgovernors.org",
+      "info@democraticsecretaries.org",
+      "info@demsofstate.org",
+      "info@derekkilmer.com",
+      "info@dga.net",
+      "info@dianeforcolorado.com",
+      "info@dickdurbin.com",
+      "info@donaldnorcrossforcongress.com",
+      "info@donnashalala.com",
+      "info@drkimschrier.com",
+      "info@durbinforsenate.com",
+      "info@dutchforcongress.com",
+      "info@dwightevans.com",
+      "info@e.alessandrabiaggi.com",
+      "info@e.charlesbooker.org",
+      "info@e.gavinnewsom.com",
+      "info@e.ronbegone.com",
+      "info@e.timryanforamerica.com",
+      "info@edmarkey.com",
+      "info@edmarkey.org",
+      "info@ehaszforcongress.com",
+      "info@elaineforcongress.com",
+      "info@electbobferguson.com",
+      "info@electdanabalter.com",
+      "info@email.ericswalwell.com",
+      "info@ericaforus.com",
+      "info@evaforcongress.com",
+      "info@fightforprogress.org",
+      "info@frankenforsenate.org",
+      "info@gallegoforarizona.com",
+      "info@gerryconnolly.com",
+      "info@ghazalahashmi.com",
+      "info@ginaortizjones.com",
+      "info@givegreen.org",
+      "info@gramm96.org",
+      "info@gretchenwhitmer.com",
+      "info@griswoldforcolorado.org",
+      "info@hakeemjeffries.com",
+      "info@halaforvirginia.com",
+      "info@haleystevensforcongress.com",
+      "info@hankforcongress.com",
+      "info@hardenforcongress.com",
+      "info@harleyforcongress.com",
+      "info@hashmi4vasenate.org",
+      "info@henrycuellar.com",
+      "info@hiralforcongress.com",
+      "info@ilhanomar.com",
+      "info@jahanahayes.com",
+      "info@jamesmackler.com",
+      "info@jaredgoldenforcongress.com",
+      "info@jaredhuffman.com",
+      "info@jasmineforcongress.com",
+      "info@jasoncrowforcongress.com",
+      "info@jeanneshaheen.org",
+      "info@jeffriesforcongress.com",
+      "info@jerrynadler.com",
+      "info@joebiden.com",
+      "info@johnkingforgovernor.com",
+      "info@jontester.com",
+      "info@joshshapiro.org",
+      "info@katieporter.com",
+      "info@kendrahorn.com",
+      "info@kusterforcongress.com",
+      "info@kyl.senate.gov",
+      "info@lucyforcongress.com",
+      "info@mad4pa.com",
+      "info@maggiehassan.com",
+      "info@mainedems.org",
+      "info@marcusforgeorgia.com",
+      "info@markkelly.com",
+      "info@markwarnerva.com",
+      "info@martinheinrich.com",
+      "info@michaelbennet.com",
+      "info@mikelevin.org",
+      "info@mikiesherrill.com",
+      "info@mjfortexas.com",
+      "info@mrvanforcongress.com",
+      "info@murphy4nj.com",
+      "info@newmexicansformichelle.com",
+      "info@ninaturner.com",
+      "info@ocasiocortez.com",
+      "info@oconnorforcongress.com",
+      "info@pelosiforcongress.org",
+      "info@perlmutterforcolorado.com",
+      "info@peteforamerica.com",
+      "info@petersformichigan.com",
+      "info@philarballo.com",
+      "info@plaskettforcongress.org",
+      "info@pramilaforcongress.com",
+      "info@putortiforcongress.com",
+      "info@quinnforcongress.com",
+      "info@reelectnydiavelazquez.com",
+      "info@richardblumenthal.com",
+      "info@rokhanna.com",
+      "info@roycooper.com",
+      "info@savedemocracypac.com",
+      "info@scanlonforcongress.com",
+      "info@scholten4iowa.com",
+      "info@shahidforchange.us",
+      "info@sherrodbrown.com",
+      "info@sitesforcongress.com",
+      "info@speakergideon.com",
+      "info@stabenowforsenate.com",
+      "info@stantonforarizona.com",
+      "info@stephaniemurphyforcongress.com",
+      "info@stevesisolak.com",
+      "info@stoneyforrva.com",
+      "info@susieleeforcongress.com",
+      "info@tammybaldwin.com",
+      "info@tedescoforcongress.com",
+      "info@tedlieu.com",
+      "info@tedracobb.com",
+      "info@teresatomlinson.com",
+      "info@tomohalleran.com",
+      "info@torres.nyc",
+      "info@valarkoosh.com",
+      "info@votegretchen.com",
+      "info@voteyvette.com",
+      "info@warnockforgeorgia.com",
+      "info@wextonforcongress.com",
+      "info@wildforcongress.com",
+      "info@zoelofgren.com",
+      "information@emilyslist.org",
+      "inquiries@chuyforcongress.com",
+      "jesse_helms@helms.senate.gov",
+      "john_ashcroft@ashcroft.senate.gov",
+      "john_kerry@kerry.senate.gov",
+      "josh@josh4congress.com",
+      "kate@betoorourke.com",
+      "kg@kirstengillibrand.com",
+      "kit_bond@bond.senate.gov",
+      "lander@council.nyc.gov",
+      "larry_craig@craig.senate.gov",
+      "lugar@iquest.net",
+      "mail_grams@grams.senate.gov",
+      "mailbox@gregg.senate.gov",
+      "matt@mattforarlington.com",
+      "max@baucus.senate.gov",
+      "mayor.garcetti@lacity.org",
+      "michigan@abraham.senate.gov",
+      "news@janschakowsky.org",
+      "nickles@rpc.senate.gov",
+      "olympia@snowe.senate.gov",
+      "opinion@smith.senate.gov",
+      "rashida@rashidaforcongress.com",
+      "RepChrissyHoulahan@mail.house.gov",
+      "reply@adamschiff.com",
+      "reply@janetmills.com",
+      "sam_brownback@brownback.senate.gov",
+      "Sandra@Agathaforcongress.com",
+      "sanfordbishop@sanfordbishop.com",
+      "sen_dodd@dodd.senate.gov",
+      "senator_al@damato.senate.gov",
+      "Senator_Bingaman@bingaman.senate.gov",
+      "Senator_Byrd@Byrd.Senate.gov",
+      "senator_chafee@chafee.senate.gov",
+      "senator_coverdell@coverdell.senate.gov",
+      "senator_dewine@dewine.senate.gov",
+      "senator_domenici@domenici.senate.gov",
+      "senator_frist@frist.senate.gov",
+      "Senator_Glenn@glenn.senate.gov",
+      "Senator_Gorton@gorton.senate.gov",
+      "senator_hatch@hatch.senate.gov",
+      "senator_kohl@kohl.senate.gov",
+      "senator_leahy@leahy.senate.gov",
+      "senator_lieberman@lieberman.senate.gov",
+      "senator_max_cleland@cleland.senate.gov",
+      "Senator_McCain@mccain.senate.gov",
+      "Senator_Merkley@Merkley.senate.gov",
+      "senator_murray@murray.senate.gov",
+      "senator_reid@reid.senate.gov",
+      "senator_specter@specter.senate.gov",
+      "senator_thompson@thompson.senate.gov",
+      "senator_torricelli@torricelli.senate.gov",
+      "senator.hutchinson@hutchinson.senate.gov",
+      "senator@bennett.senate.gov",
+      "senator@biden.senate.gov",
+      "senator@boxer.senate.gov",
+      "senator@breaux.senate.gov",
+      "senator@bryan.sen.gov",
+      "senator@bumpers.senate.gov",
+      "senator@cochran.senate.gov",
+      "senator@collins.senate.gov",
+      "senator@conrad.senate.gov",
+      "senator@dorgan.senate.gov",
+      "Senator@dpm.senate.gov",
+      "senator@enzi.senate.gov",
+      "senator@faircloth.senate.gov",
+      "senator@feingold.senate.gov",
+      "senator@feinstein.senate.gov",
+      "senator@hollings.senate.gov",
+      "senator@hutchison.senate.gov",
+      "senator@inouye.senate.gov",
+      "senator@kennedy.senate.gov",
+      "senator@landrieu.senate.gov",
+      "senator@levin.senate.gov",
+      "senator@mcconnell.senate.gov",
+      "senator@mikulski.senate.gov",
+      "senator@moseley-braun.senate.gov",
+      "senator@robb.senate.gov",
+      "senator@rockefeller.senate.gov",
+      "senator@santorum.senate.gov",
+      "senator@sarbanes.senate.gov",
+      "senator@shelby.senate.gov",
+      "senator@thurmond.senate.gov",
+      "senator@warner.senate.gov",
+      "senator@wellstone.senate.gov",
+      "senatorlott@lott.senate.gov",
+      "suzanne@bonamiciforcongress.com",
+      "tammy@tammyduckworth.com",
+      "Team@AratiForCongress.com",
+      "team@brigidforsouthjersey.com",
+      "team@her-time.com",
+      "team@joshstein.org",
+      "team@leftnet.org",
+      "team@lizziefletcher.com",
+      "team@marienewmanforcongress.com",
+      "team@sethmoulton.com",
+      "teamcori@coribush.org",
+      "teameva@evaforcongress.com",
+      "teammaloney@carolynmaloney.com",
+      "teampappas@chrispappas.org",
+      "TeamWilliams@kathleenformontana.com",
+      "tim_johnson@johnson.senate.gov",
+      "tom_daschle@daschle.senate.gov",
+      "tom_harkin@harkin.senate.gov",
+      "us@bowmanforcongress.com",
+      "us@jessicacisnerosforcongress.com",
+      "valdezforco@gmail.com,",
+      "vanhollenformd@vanhollen.org",
+      "vermont@jeffords.senate.gov",
+      "wendell_ford@ford.senate.gov",
+      "wyden@teleport.com",
+      "xavier@xavierbecerra.com",
+      ]
+    var caEmails = commonAppEmails.join(",")
+ 
+   var emailLetter2 = encodeURIComponent(fullLetter2.replace("No Toys Left Behind"))
+   var emailLink2=`mailto:?bcc=${caEmails}&subject=No Toys Left Behind&body=${emailLetter2}`
+  $('#commonAppEmails').attr("href", emailLink2) 
+
+})
+
